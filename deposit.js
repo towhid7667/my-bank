9f6408ded4c99ed0c4d016983f4da1714232b2b5
towhid7667 <towhidulhq@gmail.com>
2022-08-20 11:18:28 +0600

    Deposit amount is working fine

---
+++ deposit.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    let newdepositElement = document.getElementById('deposit-field');
+    let newDepositAmountString = newdepositElement.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    // console.log(newDepositAmount);
+
+    let previousDepositElement = document.getElementById('deposit-total');
+    let previousDepositAmountString = previousDepositElement.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountString);
+
+    let currentDepositTotal = previousDepositAmount + newDepositAmount;
+    previousDepositElement.innerText = currentDepositTotal;
+
+
+    // let us fix the balance element 
+    let previousBalanceElement = document.getElementById('balance-total');
+    let previousBalanceAmountString = previousBalanceElement.innerText;
+    let previousBalanceAmount = parseFloat(previousBalanceAmountString);
+    let currentBalanceTotal = previousBalanceAmount + newDepositAmount;
+    previousBalanceElement.innerText = currentBalanceTotal;
+
+
+
+})
